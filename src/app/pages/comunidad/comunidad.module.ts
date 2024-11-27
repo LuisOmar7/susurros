5b9958d34f10c573f38a39a78fa6c1ab4fe9fce8
HuguitoU <hugou770@gmail.com>
2024-11-26 21:22:26 -0600

    Bases comunidad

--- src/app/pages/comunidad/comunidad.module.ts
+++ src/app/pages/comunidad/comunidad.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ComunidadPageRoutingModule } from './comunidad-routing.module';
-
 import { ComunidadPage } from './comunidad.page';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ComunidadPageRoutingModule
+    ComunidadPageRoutingModule,
+    ComponentesModule
   ],
   declarations: [ComunidadPage]
 })
--- src/app/pages/comunidad/comunidad.page.ts
+++ src/app/pages/comunidad/comunidad.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-comunidad',
@@ -12,5 +11,5 @@ export class ComunidadPage implements OnInit {
 
   ngOnInit() {
   }
-
+  
 }
